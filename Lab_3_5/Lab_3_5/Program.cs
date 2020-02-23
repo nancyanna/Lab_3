6648d08f1e0b267f1b953b0291e4a062c34122b1
nancyanna <61117785+nancyanna@users.noreply.github.com>
2020-02-23 11:50:13 -0600

    Add files via upload

---
+++ Lab_3_5/Lab_3_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string blackListWorld = "viagar";
+            bool isSpam = false;
+            string message = Console.ReadLine();
+            if (message.Contains(blackListWorld))
+            {
+                isSpam = true;
+                Console.WriteLine("The message is spam");
+            }
+            else
+            {
+                isSpam = false;
+                Console.WriteLine("The message is isSpam");
+            }
+        }
+    }
+}
